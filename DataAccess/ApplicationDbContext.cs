c3258472909adab560d81d3570a0c58d38bce5a3
Huynhnghia98 <nghiaht0412@gmail.com>
2024-03-04 13:52:05 +0700

    Create Flight Docs System Project

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using FlightDocsSystem.Models;
+
+namespace FlightDocsSystem.DataAccess.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			foreach (var e in modelBuilder.Model.GetEntityTypes())
+			{
+				var tableName = e.GetTableName();
+				if (tableName.StartsWith("AspNet"))
+				{
+					e.SetTableName(tableName.Substring(6));
+				}
+			}
+
+		}
+
+	}
+}
